e203d7ab6010adeb5cfcb2e85f2dcecfca79a037
Anselm Kiefner <github@anselm.kiefner.de>
2025-07-01 21:08:09 +0200

    Add __init__.py to expose main module functions for public API

---
+++ src/zvic/__init__.py
@@ -0,0 +1,13 @@
+from .main import (
+    canonicalize,
+    canonicalized_to_json,
+    is_compatible,
+    json_to_canonicalized,
+)
+
+__all__ = [
+    "canonicalize",
+    "is_compatible",
+    "canonicalized_to_json",
+    "json_to_canonicalized",
+]
