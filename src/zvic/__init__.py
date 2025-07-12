ffff7dd41b4f11668bbc404b2822c02427a370ef
Anselm Kiefner <github@anselm.kiefner.de>
2025-07-12 12:07:34 +0200

    Remove unused exports from __init__.py and clean up __all__ declaration

--- src/zvic/__init__.py
+++ src/zvic/__init__.py
@@ -1,15 +1,8 @@
 from .main import (
     canonical_signature,
     canonicalize,
-    canonicalized_to_json,
     is_compatible,
-    json_to_canonicalized,
 )
+from .utils import _
 
-__all__ = [
-    "canonicalize",
-    "is_compatible",
-    "canonicalized_to_json",
-    "json_to_canonicalized",
-    "canonical_signature",
-]
+__all__ = ["canonicalize", "is_compatible", "canonical_signature", "_"]
