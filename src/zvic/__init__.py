b4519568cb7a2dd6ad011f1bed6a861afb163c13
Anselm Kiefner <github@anselm.kiefner.de>
2025-07-24 14:35:15 +0200

    Remove unused fixtures from conftest.py and update __init__.py to include compatibility imports

--- src/zvic/__init__.py
+++ src/zvic/__init__.py
@@ -1,3 +1,7 @@
+from .compatibility import is_signature_compatible
+from .compatibility_params import are_params_compatible
+from .compatibility_types import is_type_compatible
+from .exception import SignatureIncompatible
 from .main import (
     canonical_signature,
     canonicalize,
@@ -12,4 +16,8 @@ __all__ = [
     "canonical_signature",
     "_",
     "constrain_this_module",
+    "are_params_compatible",
+    "SignatureIncompatible",
+    "is_signature_compatible",
+    "is_type_compatible",
 ]
