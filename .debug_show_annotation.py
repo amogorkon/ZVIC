aeff965e681f5331786025208c003eda0d2cb2de
Anselm Kiefner <github@anselm.kiefner.de>
2025-09-03 13:40:39 +0200

    cleanup

--- .debug_show_annotation.py
+++
@@ -1,9 +0,0 @@
-import textwrap
-
-from zvic.annotation_constraints import apply_annotation_constraints
-
-src = textwrap.dedent("""
-def foo(x: list[int(0 < _ < 100)](len(_) == 3)) -> None:
-    pass
-""")
-print(apply_annotation_constraints(src))
